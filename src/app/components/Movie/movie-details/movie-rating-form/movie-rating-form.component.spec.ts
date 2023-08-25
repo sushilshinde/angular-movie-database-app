81cbe2343fbcc91d81c6abf866a5b14767365219
aswathi-suseelan <asuseelan@msystechnologies.com>
2023-08-25 20:35:39 +0530

    added test cases for movie details/ratings/favorite-list

--- src/app/components/Movie/movie-details/movie-rating-form/movie-rating-form.component.spec.ts
+++ src/app/components/Movie/movie-details/movie-rating-form/movie-rating-form.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { StoreModule } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
+import { of, Subscription } from 'rxjs';
 import { allMovieReducer } from 'src/app/core/reducers/allMovies.reducer';
 import { moviesReducer } from 'src/app/core/reducers/movies.reducers';
 import { usersReducer } from 'src/app/core/reducers/users.reducers';
@@ -16,17 +17,84 @@ describe('MovieRatingFormComponent', () => {
   let component: MovieRatingFormComponent;
   let fixture: ComponentFixture<MovieRatingFormComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  let mockStore: any;
+  let mockSnackBar: any;
+
+  beforeEach(async() => {
+
+    mockStore = {
+      select: jasmine.createSpy('select').and.returnValue(of({ activeUser: { username: 'testUser' } })),
+      dispatch: jasmine.createSpy('dispatch')
+    };
+
+    mockSnackBar = {
+      open: jasmine.createSpy('open')
+    };
+
+    await TestBed.configureTestingModule({
       declarations: [MovieRatingFormComponent, MovieRatingComponent],
-      imports: [StoreModule.forRoot({ allmovies: allMovieReducer, users: usersReducer }), MatSnackBarModule, ReactiveFormsModule, MatIconModule, RouterTestingModule]
-    });
+      imports: [StoreModule.forRoot({ allmovies: allMovieReducer, users: usersReducer }), MatSnackBarModule, ReactiveFormsModule, MatIconModule, RouterTestingModule],
+    
+      providers: [
+        { provide: Store, useValue: mockStore },
+        { provide: MatSnackBar, useValue: mockSnackBar }
+      ]
+    }).compileComponents();
+    // fixture = TestBed.createComponent(MovieRatingFormComponent);
+    // component = fixture.componentInstance;
+    // component.movieId = 457332;
+
+    // fixture.detectChanges();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(MovieRatingFormComponent);
     component = fixture.componentInstance;
+    component.movieId = 457332;
     fixture.detectChanges();
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize the reviewForm with required controls', () => {
+    const commentControl = component.reviewForm.get('comment') as FormControl;
+    expect(commentControl).toBeTruthy();
+    expect(commentControl.validator).toBeTruthy(); 
+    expect(commentControl.hasValidator(Validators.required)).toEqual(true)
+    // expect(commentControl.hasValidator(Validators.minLength(10))).toEqual(true)
+  });
+
+  it('should get rating', () => {
+    component.getRating(3);
+    expect(component.rating).toBe(3);
+  });
+
+  it('should submit review', () => {
+    const mockComment = 'This is a test comment.';
+    component.rating = 4;
+    const commentControl = component.reviewForm.get('comment') as FormControl;
+    commentControl.setValue(mockComment);
+
+    component.submitReview();
+
+    expect(mockStore.dispatch).toHaveBeenCalled();
+    expect(mockStore.dispatch).toHaveBeenCalledWith(jasmine.objectContaining({
+      type: '[movies] add review'
+    }));
+    expect(mockSnackBar.open).toHaveBeenCalled();
+    expect(mockSnackBar.open).toHaveBeenCalledWith('You rated 4 / 5', '', {
+      duration: 5000,
+    });
+  });
+
+  it('should unsubscribe from userStoreSubscriber on ngOnDestroy', () => {
+    spyOn(<Subscription>component.userStoreSubscriber, "unsubscribe");
+    component.ngOnDestroy();
+    expect(component.userStoreSubscriber?.unsubscribe).toHaveBeenCalled();
+  });
+
+
 });
--- src/app/components/favorite-list/favorite-list.component.spec.ts
+++ src/app/components/favorite-list/favorite-list.component.spec.ts
@@ -1,36 +1,112 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
-import { StoreModule } from '@ngrx/store';
-import { allMovieReducer } from 'src/app/core/reducers/allMovies.reducer';
-import { usersReducer } from 'src/app/core/reducers/users.reducers';
+import { Store, StoreModule } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { MovieDetailsModel } from 'src/app/core/interface/movie.interface';
+import data from '../Movie/data.json'
 
 import { FavoriteListComponent } from './favorite-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 describe('FavoriteListComponent', () => {
   let component: FavoriteListComponent;
   let fixture: ComponentFixture<FavoriteListComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  let mockStore: any;
+
+  beforeEach(async() => {
+
+    mockStore = {
+      select: jasmine.createSpy('select').and.returnValue({
+        subscribe: (callback: any) => {
+          callback({});
+          return {
+            unsubscribe: jasmine.createSpy('unsubscribe')
+          };
+        }
+      })
+    };
+
+    await TestBed.configureTestingModule({
       declarations: [FavoriteListComponent],
-      imports: [
-        HttpClientModule,
-        StoreModule.forRoot({
-          allmovies: allMovieReducer,
-          users: usersReducer,
-        }),
-        RouterTestingModule,
-      ],
-      providers:[MatSnackBar]
-    });
+      imports: [StoreModule.forRoot({}), RouterTestingModule, HttpClientModule, MatSnackBarModule],
+      providers: [
+        { provide: Store, useValue: mockStore }
+      ]
+    }).compileComponents();
+    // fixture = TestBed.createComponent(FavoriteListComponent);
+    // component = fixture.componentInstance;
+    // fixture.detectChanges();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(FavoriteListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should retrieve favorite movies from the store', () => {
+    // Set up mock data for activeUser and movies in the store
+    const mockActiveUser = { favorite_list: [457332, 346698] };
+    const mockMovies: MovieDetailsModel[] = data.movies.map((movie) => ({
+      ...movie,
+      ratings: [],
+      rating: 3
+    }));
+    mockStore.select.and.returnValue({
+      subscribe: (callback: any) => {
+        callback({ activeUser: mockActiveUser });
+        return {
+          unsubscribe: jasmine.createSpy('unsubscribe')
+        };
+      }
+    });
+
+    // Set up the movie store mock data
+    mockStore.select.withArgs('movies').and.returnValue({
+      subscribe: (callback: any) => {
+        callback({ movies: mockMovies });
+        return {
+          unsubscribe: jasmine.createSpy('unsubscribe')
+        };
+      }
+    });
+
+    // Trigger ngOnInit
+    component.ngOnInit();
+
+    expect(component.myFavorites).toEqual([
+      mockMovies[0],
+      mockMovies[1]
+    ]);
+  });
+
+  it('should unsubscribe from userStoreSubscription and movieStoreSubscription on ngOnDestroy', () => {
+    // if(component.userStoreSubscription) {
+    //   spyOn(component.userStoreSubscription, 'unsubscribe');
+    // }
+
+    // if(component.movieStoreSubscripption) {
+    //   spyOn(component.movieStoreSubscripption, 'unsubscribe');
+    // }
+    component.ngOnDestroy();
+    
+    if(component.userStoreSubscription) {
+      expect(component.userStoreSubscription?.unsubscribe).toHaveBeenCalled();
+
+    }
+
+    if(component.movieStoreSubscripption) {
+
+      expect(component.movieStoreSubscripption?.unsubscribe).toHaveBeenCalled();
+    }
+    
+  });
+
 });
